525b4d8d0bdc1da0b8416e19791e731b0ea2028d
nikhil74589 <nikhilranjan2501@gmail.com>
2026-07-08 14:36:09 +0530

    Smoke Pipeline

--- init.py
+++ init.py
@@ -1,48 +1,43 @@
 import os
 import boto3
+import sys
 import pymysql
 
-client = boto3.client("ssm", region_name="us-east-2")
+client=boto3.client("ssm",region_name="us-east-1")
 
-def get_params(name):
-    response = client.get_parameter(
+def get_param(name):
+    return client.get_parameter(
         Name=f"/application/banking/{name}",
         WithDecryption=True
-    )
-    return response["Parameter"]["Value"]
-
-conn = None
+    )["Parameter"]["Value"]
 
 try:
-    conn = pymysql.connect(
-        host=get_params("DB_HOST"),
-        user=get_params("DB_USER"),
-        password=get_params("DB_PASSWORD"),
-        port=int(get_params("DB_PORT")),
+    conn=pymysql.connect(
+        host=get_param("DB_HOST"),
+        port=int(get_param("DB_PORT")),
+        user=get_param("DB_USER"),
+        password=get_param("DB_PASSWORD"),
         connect_timeout=10,
     )
 
-    cur = conn.cursor()
+    cur=conn.cursor()
 
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    sql_file = os.path.join(base_dir, "init.sql")
+    base_dir=os.path.dirname(os.path.abspath(__file__))
+    sql_file=os.path.join(base_dir,"init.sql")
 
-    with open(sql_file, "r", encoding="utf-8") as f:
-        sql = f.read()
+    with open(sql_file,"r",encoding="utf-8") as f:
+        sql=f.read()
 
     for statement in sql.split(";"):
-        statement = statement.strip()
+        statement=statement.strip()
         if statement:
             cur.execute(statement)
-
+    
     conn.commit()
-    cur.close()
-
-    print("✅ Database initialized successfully")
+    print("✅ Database connected successfully")
 
 except Exception as e:
-    print("Error:", e)
+    print("Error :",e)
 
 finally:
-    if conn:
-        conn.close()
+    conn.close()
