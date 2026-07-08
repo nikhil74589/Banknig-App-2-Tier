db1575e22d23cc369a37773497db3e9a917d3dc7
nikhil74589 <nikhilranjan2501@gmail.com>
2026-07-08 14:42:45 +0530

    Smoke Pipeline

--- smoke_test.py
+++ smoke_test.py
@@ -35,7 +35,6 @@ try:
         host=params["DB_HOST"],
         user=params["DB_USER"],
         password=params["DB_PASSWORD"],
-        database=params["DB_NAME"],
         port=int(params["DB_PORT"]),
         connect_timeout=10
     )
